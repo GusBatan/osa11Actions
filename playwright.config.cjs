ed6685ea2ef1241315547eacabd9edc484d9de8d
Tuomas <register@auroracode.fi>
2025-01-11 18:38:26 +0200

    try fix

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -71,7 +71,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm start',
+    command: 'npm run start',
     url: 'http://127.0.0.1:3001',
     timeout: 60 * 1000,
     reuseExistingServer: !process.env.CI,
